3ffa4279a5e6e71cb0af79cf4b571cc8fb6a593f
Camdaman21 <cameronmm21@icloud.com>
2023-10-20 09:25:52 -0400

    little more work on camera

--- steampunkRTS/Assets/Scripts/CameraController.cs
+++ steampunkRTS/Assets/Scripts/CameraController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public float movementSpeed;
+    public float movementTime;
+
+    public Vector3 newPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        newPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void HandleMovementInput()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            newPosition += (transform.forward * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            newPosition += (transform.forward * -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            newPosition += (transform.right * movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            newPosition += (transform.right * -movementSpeed);
+
+        }
+
     }
 }
