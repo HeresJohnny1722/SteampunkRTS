e6eeb771a05a1e41207c2a29c9d510ac7bab5e6f
HeresJohnny1722 <jonathan.krolak@icloud.com>
2023-10-21 21:23:15 -0400

    UnitSelection

--- steampunkRTS/Assets/UnitSelections.cs
+++ steampunkRTS/Assets/UnitSelections.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class UnitSelections : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> unitList = new List<GameObject>();
+    public List<GameObject> unitsSelected = new List<GameObject>();
+
+    private static UnitSelections _instance;
+    public static UnitSelections Instance { get { return _instance; } }
+
+    void Awake()
     {
-        
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ClickSelect(GameObject unitToAdd)
     {
-        
+
+    }
+
+    public void ShiftClickSelect(GameObject unitToAdd)
+    {
+
+    }
+
+    public void DragSelect(GameObject unitToAdd)
+    {
+
+    }
+
+    public void DeselectAll()
+    {
+
+    }
+
+    public void Deselect(GameObject unitToDeselect)
+    {
+
     }
 }
