411d5bb0ee3d02cddb9c5c379f8141dcfbd34693
Camdaman21 <cameronmm21@icloud.com>
2023-10-24 08:54:42 -0400

    building stuff

---
+++ steampunkRTS/Assets/Scripts/PlacementSystem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlacementSystem : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject mouseIndicator;
+    
+    public InputManager inputManager;
+
+    
+
+    private void Update()
+    {
+        Vector3 mousePosition = inputManager.GetSelectedMapPosition();
+        mouseIndicator.transform.position = mousePosition;
+    }
+}
