90d811a8f87b8ec73f8d6ea613093db49a6b886d
HeresJohnny1722 <jonathan.krolak@icloud.com>
2023-11-02 12:49:23 -0400

    setting up a scene

---
+++ steampunkRTS/Assets/TestUI.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TestUI : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+
+    public void testUi()
+    {
+        text.text = "Changed text";
+        Debug.Log("hello");
+    }
+    
+}
